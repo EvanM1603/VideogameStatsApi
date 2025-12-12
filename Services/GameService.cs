a02989c7c639728bd709a59b32a7f04187c9a212
EvanM1603 <evanmcdonnell09@gmail.com>
2025-12-12 18:56:19 +0000

    Fixed issue when connecting to DB in appsettings.json, connection was wrong.

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -1,21 +1,14 @@
 ﻿using VideogameStatsApi.Models;
+using VideogameStatsApi.Data;
+using Microsoft.EntityFrameworkCore;
 
 // Last working on this
 
 namespace VideogameStatsApi.Services
 {
-    public class GameService : IGameService
+    public class GameService(AppDbContext context) : IGameService
     {
         // Reference: Created GameService - https://youtu.be/RwQVRXEs370?si=N1sEu7UzYicQTNFa&t=1885
-
-        //Reference: Moved test data to Service -  https://youtu.be/RwQVRXEs370?si=DwAX1WHKcdHjEZcH&t=1906
-
-        static List<Game> games = new List<Game>{
-            new Game { Id = 1, Name = "Valorant" },
-            new Game { Id = 2, Name = "Rainbow Six Siege" },
-            new Game { Id = 3, Name = "CS:GO" }
-        };
-
 
         public Task<Game> AddGameAsync(Game game)
         {
@@ -28,12 +21,12 @@ namespace VideogameStatsApi.Services
         }
 
         public async Task<List<Game>> GetAllGamesAsync()
-        => await Task.FromResult(games);
+        => await context.Games.ToListAsync();
 
         public async Task<Game?> GetGameByIdAsync(int id)
         {
-            var result = games.FirstOrDefault(x => x.Id == id);
-            return await Task.FromResult(result);
+            var result = await context.Games.FindAsync(id);
+            return result;
         }
 
         public Task<Game> GetGameByNameAsync()
