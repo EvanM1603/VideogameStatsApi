c1e5f94b6bc5e54579dc84f6bb4c03cb649b8b6e
EvanM1603 <evanmcdonnell09@gmail.com>
2025-12-15 20:31:05 +0000

    Updated the AppDbContext to include Players, Matches and PlayerMatchStats, this way I can create the rest of the CRUD functionality.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,5 +6,8 @@ namespace VideogameStatsApi.Data
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         public DbSet<Game> Games => Set<Game>();
+        public DbSet<Player> Players => Set<Player>();
+        public DbSet<Match> Matches => Set<Match>();
+        public DbSet<PlayerMatchStat> PlayerMatchStats => Set<PlayerMatchStat>();
     }
 }
