b3ca6faaeb0a3ceffb76fc853502d21d964cbb99
EvanM1603 <evanmcdonnell09@gmail.com>
2025-12-17 21:24:05 +0000

    Created the Match Dtos, referenced and commented them

---
+++ Dtos/CreateMatchRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace VideogameStatsApi.Dtos
+{
+    //Reference: Used to Create Dtos - https://youtu.be/RwQVRXEs370?si=J5DyOYwsu2vnNrvn&t=3538
+    // DTO used to create a new Match
+    public class CreateMatchRequest
+    {
+        // The ID of the Match
+        public int MatchId { get; set; }
+        // The ID of the Game the Match was played on
+        public int GameId { get; set; }
+        // The Map the Match was played on
+        public string Map {  get; set; } = string.Empty;
+        // The Result of the Match
+        public string Result { get; set; } = string.Empty;
+    }
+}
---
+++ Dtos/UpdateMatchRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace VideogameStatsApi.Dtos
+{
+    //Reference: Used to Create Dtos - https://youtu.be/RwQVRXEs370?si=J5DyOYwsu2vnNrvn&t=3538
+    // DTO used to update an existing Match
+    public class UpdateMatchRequest
+    {
+        // The ID of the Match
+        public int MatchId { get; set; }
+        // The ID of the Game the Match was played on
+        public string Map { get; set; } = string.Empty;
+        // The Result of the Match
+        public string Result { get; set; } = string.Empty;
+    }
+}
